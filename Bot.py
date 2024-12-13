9cb4c4f7b650eeb02c569b35f6b01f5fb84851ce
zulroxx <42370631+zulroxx@users.noreply.github.com>
2024-12-14 05:13:22 +0700

    Update Bot.py
    Change token

--- Bot.py
+++ Bot.py
@@ -6,7 +6,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("https://t.me/+2-1JeRuUBDtlMmZl")
 
 # Main application setup
-app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()  # Replace YOUR_BOT_TOKEN with your bot's token
+app = ApplicationBuilder().token("7601384842:AAGpEogD6U5RkDIJsyYveQSFiXn6JC1R1vo").build()  # Replace YOUR_BOT_TOKEN with your bot's token
 
 # Add the command handler
 app.add_handler(CommandHandler("start", start))
